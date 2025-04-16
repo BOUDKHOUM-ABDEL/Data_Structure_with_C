e4eeef139792634069095912eb5c7c30e10ee1fd
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-16 23:54:12 +0100

    add structs

--- Queue using Linked list operations in C/FILE.h
+++ Queue using Linked list operations in C/FILE.h
@@ -1,1 +1,17 @@
+#ifndef PILE_H_INCLUDED
+#define PILE_H_INCLUDED
 
+typedef struct{
+   int data;
+   struct node* next ;
+}node;
+
+typedef struct File{
+   node* tete ;
+   node* queue;
+   int taille;
+}File;
+
+
+
+#endif // PILE_H_INCLUDED
