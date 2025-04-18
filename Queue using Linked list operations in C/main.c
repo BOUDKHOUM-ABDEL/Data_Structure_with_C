707a404fae5a52afb7d4218c42553eec481aa771
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-19 00:17:54 +0100

    Update main.c

--- Queue using Linked list operations in C/main.c
+++ Queue using Linked list operations in C/main.c
@@ -8,6 +8,21 @@ int main()
    int val;
    int choix;
    init_file(&F1);
+   
+  do{
+
+       puts("------------------menu-------------------");
+       printf("1.ajouter une valeur la file \n");
+       printf("2.supprimer la tete a la file \n");
+       printf("3.afficher la file \n");
+       printf("0.quitte \n");
+       puts("------------------menu-------------------");
+
+
+       printf("choisis votre modification:\n \n");
+       scanf("%d",&choix);
+       
+    } while (choix != 0);
 
  
     return 0;
